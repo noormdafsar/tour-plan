157fc6f76502d6133c8714bd30507242999fbb77
noormdafsar <nooruddin.mdafsar.it25@heritageit.edu.in>
2023-06-29 21:16:45 +0530

    Initial commit

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -3,7 +3,7 @@ function Tours({tours, removeTour}) {
     return (
         <div className='container'>
             <div>
-                <h2 className='title'> Plan With Love</h2>
+                <h2 className='title'>Plan Your Tour</h2>
             </div>
             <div className='cards'>
                 {
